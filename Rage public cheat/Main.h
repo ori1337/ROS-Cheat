7f9fa949ab65f3f8dc4b6bba0409632ffed62476
ori1337 <orikeshet1337@gmail.com>
2018-06-29 11:30:42 -0700

    kl
    kl

--- Rage public cheat/Main.h
+++ Rage public cheat/Main.h
@@ -27,6 +27,7 @@ using namespace std;
 
 #pragma warning (disable: 4244) //
 #pragma warning (disable: 4996)
+#pragma warning (disable: 4838)
 #define _CRT_SECURE_NO_DEPRECATE
 
 //==========================================================================================================================
